cd2aee5cd0ed6c6988a9ec5b3cc7bea9ba8a8607
Justin Garrison <justinleegarrison@gmail.com>
2023-02-10 23:47:06 -0800

    woops

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -4,8 +4,8 @@
 export async function onRequestPost(context) {
     try {
         let email = await context.request.formData();
-        await SUBS.put(input[0], "true");
-        let pretty = JSON.stringify([...input], null, 2);
+        await SUBS.put(email[0], "true");
+        let pretty = JSON.stringify([...email], null, 2);
         return new Response(pretty, {
             headers: {
                 'Content-Type': 'application/json;charset=utf-8',
