bf4d2e12c7afb72fec2c4d6825b5dce8a7f6b5cb
Justin Garrison <justinleegarrison@gmail.com>
2023-02-16 23:32:33 -0800

    maybe this

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -4,10 +4,10 @@
 export async function onRequestPost(context) {
     try {
         let email = await context.request.formData();
-        // await context.env.SUBS.put(email, "true");
+        await context.env.SUBS.put(email.entries()[1], "true");
         // let pretty = JSON.stringify(email, null, 2);
         let body = Array.from(email.entries());
-        return new Response("Thank you for subscribing" + body, { status: 200 });
+        return new Response("Thank you for subscribing " + body[1], { status: 200 });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
