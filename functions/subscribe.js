b8776489128e3f719d0d14c08f23ee12d0efcdcb
Justin Garrison <justinleegarrison@gmail.com>
2023-02-16 23:40:42 -0800

    please

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -3,11 +3,10 @@
  */
 export async function onRequestPost(context) {
     try {
-        let email = await context.request.formData();
-        await context.env.SUBS.put(email.entries()[0], "true");
-        // let pretty = JSON.stringify(email, null, 2);
-        let body = Array.from(email.entries());
-        return new Response("Thank you for subscribing " + body[0], { status: 200 });
+        let email = await context.request.formData().entries().split(",")[1];
+        await context.env.SUBS.put(email, "true");
+
+        return new Response("Thank you for subscribing " + email, { status: 200 });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
