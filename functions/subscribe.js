86f3afeab4a713eb3ec00c890f81d7228cccfedb
Justin Garrison <justinleegarrison@gmail.com>
2023-02-19 22:50:03 -0800

    back to just body

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -7,11 +7,11 @@ export async function onRequestPost(context) {
         let body = Array.from(email.entries());
         console.log(body);
 
-        emailAddress = body.toString().replace("email,", "")
-        console.log(emailAddress);
+        // emailAddress = body.toString().replace("email,", "")
+        // console.log(emailAddress);
 
         await context.env.SUBS.put(body, "true");
-        return new Response("Thank you for subscribing " + body + " or " + emailAddress, { status: 200 });
+        return new Response("Thank you for subscribing " + body, { status: 200 });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
