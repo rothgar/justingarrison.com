f5135fbe0e62fa27fa57afa242170b26d44cae4b
Justin Garrison <justinleegarrison@gmail.com>
2023-02-19 23:21:21 -0800

    this should work

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -3,19 +3,19 @@
  */
 export async function onRequestPost(context) {
     try {
-        let email = await context.request.formData();
-        let body = JSON.stringify(Array.from(email.entries())[0]);
+        let emailForm = await context.request.formData();
+        let body = JSON.stringify(Array.from(emailForm.entries())[0]);
         console.log(typeof body);
         let bodyType = typeof (body);
 
         let bodyArray = body.split(",");
-        let emailAddress = bodyArray[1].replace("]", "");
+        let emailAddress = bodyArray[1].replace("]", "").replace(/(^"|"$)/g, '');
 
         // emailAddress = body.toString().replace("email,", "")
         // console.log(emailAddress);
 
-        await context.env.SUBS.put(body, "true");
-        return new Response("Thank you for subscribing " + body + " email " + emailAddress, { status: 200 });
+        await context.env.SUBS.put(emailAddress, "true");
+        return new Response("Thank you for subscribing " + emailAddress, { status: 200 });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
