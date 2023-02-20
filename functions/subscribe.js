68db573fb58f124a5af296b6f8b1637ba2c86b1a
Justin Garrison <justinleegarrison@gmail.com>
2023-02-19 21:53:56 -0800

    roll back test function

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -3,13 +3,13 @@
  */
 export async function onRequestPost(context) {
     try {
-        let rawForm = await context.request.formData()
-        let entries = rawForm.entries();
-        let entriesA = entries.split(",");
-        let email = rawForm[1];
-        await context.env.SUBS.put(email, "true");
+        let email = await context.request.formData();
 
-        return new Response("Thank you for subscribing " + email, { status: 200 });
+        await context.env.SUBS.put(email.entries(), "true");
+        let pretty = JSON.stringify(email, null, 2);
+        console.log(pretty);
+        let body = Array.from(email.entries());
+        return new Response("Thank you for subscribing " + body, { status: 200 });
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
