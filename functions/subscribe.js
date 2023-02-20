7d43618f1e7c8410de5528b6bd212bf63af0de10
Justin Garrison <justinleegarrison@gmail.com>
2023-02-19 22:04:43 -0800

    form object update

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -5,11 +5,11 @@ export async function onRequestPost(context) {
     try {
         let email = await context.request.formData();
 
-        await context.env.SUBS.put(email.entries(), "true");
         let pretty = JSON.stringify(email, null, 2);
-        console.log(pretty);
+        await context.env.SUBS.put(pretty, "true");
         let body = Array.from(email.entries());
         return new Response("Thank you for subscribing " + body, { status: 200 });
+        console.log(pretty);
     } catch (err) {
         return new Response('Error parsing JSON content', { status: 400 });
     }
