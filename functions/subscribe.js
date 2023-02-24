e8fe4e34295497cc4bee789e699815088df4efd2
Justin Garrison <justinleegarrison@gmail.com>
2023-02-23 21:16:50 -0800

    additional logging

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -7,24 +7,26 @@ export async function onRequestPost(context) {
         console.log(c);
 
         let emailForm = await context.request.formData();
-        // come out as ["email","<email>"]
+        // come out as [["email","<email>"],["referrer","<url>"]]
         let emailData = JSON.stringify(Array.from(emailForm.entries())[0]);
 
         let emailArray = emailData.split(",");
         let emailAddress = emailArray[1].replace("]", "").replace(/(^"|"$)/g, '');
+        console.log("after email");
 
         // get referrer
         let refData = JSON.stringify(Array.from(emailForm.entries()[1]));
         let refArray = refData.split(",");
         let refAddress = refArray[1].replace("]", "").replace(/(^"|"$)/g, '');
+        console.log("after referrer")
 
         console.log("email: " + emailAddress);
         console.log("referrer: " + refAddress);
         await context.env.SUBS.put(emailAddress, "true", {
             metadata: { lastUpdate: Date() },
         });
-        return Response.redirect("https://justingarrison.com/?email=submit", 301);
+        return new Response.redirect("https://justingarrison.com/?email=submit", 301);
     } catch (err) {
-        return Response('Error parsing JSON content', { status: 400 });
+        return new Response('Error parsing JSON content', { status: 400 });
     }
 }
