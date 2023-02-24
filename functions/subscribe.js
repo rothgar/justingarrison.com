1bd81502a5ab7b8c1fa58d9092a9ffb1a601acef
Justin Garrison <justinleegarrison@gmail.com>
2023-02-23 22:12:32 -0800

    stepping

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -8,17 +8,19 @@ export async function onRequestPost(context) {
 
         let emailForm = await context.request.formData();
         // come out as [["email","<email>"],["referrer","<url>"]]
+        console.log("first");
         let formData = JSON.stringify(Array.from(emailForm.entries()));
+        console.log("second");
         let formArray = formData.split(",");
 
+        console.log("here");
         // get email
         // remove ", [, ]
         let emailAddress = formArray[1].replace(/["\[\]]/g, '');
-
+        console.log("there");
         // get referrer
         let refAddress = formArray[3].replace(/["\[\]]/g, '');
 
-        // console.log(refArray);
         console.log("email: " + emailAddress);
         console.log("referrer: " + refAddress);
         await context.env.SUBS.put(emailAddress, "true", {
