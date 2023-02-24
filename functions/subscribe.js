8ac745f0dcf7722bdcad0ce38475e4c2b65df899
Justin Garrison <justinleegarrison@gmail.com>
2023-02-23 17:23:18 -0800

    Update logging

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -13,7 +13,7 @@ export async function onRequestPost(context) {
         let bodyArray = body.split(",");
         let emailAddress = bodyArray[1].replace("]", "").replace(/(^"|"$)/g, '');
 
-        console.log(new Map(context.headers))
+        console.log(JSON.stringify(context.headers));
 
         console.log("email: " + emailAddress)
         await context.env.SUBS.put(emailAddress, "true", {
